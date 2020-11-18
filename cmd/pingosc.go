3cea341c851158383bdfb35c057198894413f6c3
Douglas Finlay <douglas@douglasfinlay.com>
2020-11-18 22:09:13 +0000

    Implement ping

--- cmd/pingosc.go
+++ cmd/pingosc.go
@@ -4,9 +4,17 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"sync"
 	"time"
 
+	"github.com/go-ping/ping"
 	"gopkg.in/yaml.v2"
+)
+
+const (
+	pingResponseTimeout = 5 * time.Second
+	pingPacketInterval  = 1 * time.Second
+	packetLossThreshold = 50
 )
 
 type eosConsole struct {
@@ -19,6 +27,9 @@ type host struct {
 	Addr  string `yaml:"ip"`
 	OscUp string `yaml:"oscUp"`
 	OscDn string `yaml:"oscDn"`
+
+	pinger *ping.Pinger
+	isUp   bool
 }
 
 type appConfig struct {
@@ -26,8 +37,6 @@ type appConfig struct {
 	Console             eosConsole `yaml:"eosConsole"`
 	Hosts               []host     `yaml:"hosts"`
 }
-
-var config appConfig
 
 func main() {
 	if len(os.Args) < 2 {
@@ -45,43 +54,110 @@ func main() {
 
 	decoder := yaml.NewDecoder(configFile)
 	decoder.SetStrict(true)
+
+	var config appConfig
 	err = decoder.Decode(&config)
 	if err != nil {
 		fmt.Printf("Failed to parse config: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(config)
-
 	if len(config.Hosts) == 0 {
 		fmt.Println("Please specify at least one host entry")
 		os.Exit(1)
 	}
 
-	quit := make(chan struct{})
+	fmt.Println("Running...")
 
-	// Catch ctrl+c
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt)
+	duration := time.Duration(config.PingIntervalSeconds) * time.Second
+	ticker := time.NewTicker(duration)
+	stop := make(chan struct{})
+
 	go func() {
-		<-sig
-		close(quit)
+		for {
+			createPingers(config.Hosts)
+			runPingers(config.Hosts)
+			printResults(config.Hosts)
+
+			select {
+			case <-ticker.C:
+				continue
+			case <-stop:
+				ticker.Stop()
+				return
+			}
+		}
 	}()
 
-	ticker := time.NewTicker(time.Duration(config.PingIntervalSeconds) * time.Second)
-	for {
-		pingHosts()
+	// Wait for SIGINT
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	<-sig
 
-		select {
-		case <-ticker.C:
+	fmt.Println("Stopping...")
+}
+
+func createPingers(hosts []host) []ping.Pinger {
+	var pingers []ping.Pinger
+
+	for i, host := range hosts {
+		pinger, err := ping.NewPinger(host.Addr)
+		if err != nil {
+			fmt.Printf("Failed to create pinger for %v (%v)", host.Name, host.Addr)
 			continue
-		case <-quit:
-			ticker.Stop()
-			return
 		}
+
+		hosts[i].pinger = pinger
+		pinger.RecordRtts = false
+		pinger.Interval = pingPacketInterval
+		pinger.Count = 3
+		pinger.Timeout = pingResponseTimeout
+	}
+
+	return pingers
+}
+
+func runPingers(hosts []host) {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	results := make(map[string]bool)
+
+	for _, host := range hosts {
+		if host.pinger == nil {
+			fmt.Println("pinger is null")
+			continue
+		}
+
+		host.pinger.OnFinish = func(stats *ping.Statistics) {
+			isUp := stats.PacketLoss <= packetLossThreshold
+			mu.Lock()
+			results[stats.Addr] = isUp
+			mu.Unlock()
+			wg.Done()
+		}
+
+		wg.Add(1)
+		go host.pinger.Run()
+	}
+
+	wg.Wait()
+
+	for i := range hosts {
+		addr := hosts[i].Addr
+		hosts[i].isUp = results[addr]
 	}
 }
 
-func pingHosts() {
-	fmt.Print(".")
+func printResults(hosts []host) {
+	for _, host := range hosts {
+		var msg string
+		if host.isUp {
+			msg = "online"
+		} else {
+			msg = "offline"
+		}
+		fmt.Printf("%-20s%s\n", host.Name, msg)
+	}
+
+	fmt.Println("---------------------------")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dougfinl/pingosc
 
 go 1.15
 
-require gopkg.in/yaml.v2 v2.3.0
+require (
+	github.com/go-ping/ping v0.0.0-20201115131931-3300c582a663
+	gopkg.in/yaml.v2 v2.3.0
+)
